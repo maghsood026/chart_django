bf79d4f5378c118e8722b435e6955988a95246d8
maghsood <maghsood026@yahoo.com>
2020-04-06 00:31:36 +0430

    another try

--- charts/settings.py
+++ charts/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,7 +126,6 @@ STATIC_DIR = [
     os.path.join(BASE_DIR, STATIC_URL)
 ]
 
-import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
